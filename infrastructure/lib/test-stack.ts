61190e5375e09702c7b5225a400c7b551ab7e3d1
Kamron Bhavnagri <kamwithk@tuta.io>
2023-07-15 12:16:15 +1000

    Improved lambda go path

--- infrastructure/lib/test-stack.ts
+++ infrastructure/lib/test-stack.ts
@@ -1,13 +1,14 @@
 import { GoFunction } from '@aws-cdk/aws-lambda-go-alpha';
 import { Stack } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import path = require('path');
 
 export class TestStack extends Stack {
     constructor(scope: Construct, id: string) {
         super(scope, id);
 
         const testFunction = new GoFunction(this, 'testFunction', {
-            entry: '../functions/test',
+            entry: path.join(__dirname, '../../functions/test'),
             functionName: 'testFunction',
         });
     }
